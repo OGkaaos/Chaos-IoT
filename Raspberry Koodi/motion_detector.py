a9f6a845e19564b2569291dac9c2f5215e63e394
Marcos Raudkett <info@marcosraudkett.com>
2017-11-27 16:27:07 +0200

    Update motion_detector.py

--- Raspberry Koodi/motion_detector.py
+++ Raspberry Koodi/motion_detector.py
@@ -14,14 +14,18 @@ from resizeimage import resizeimage # Jotta voidaan vaihtaa kuvan kokoa
 camera = picamera.PiCamera() # PiCamera
 pin = 18                     # Liiketunnistimen PIN -> 18
 
-GPIO.setmode(GPIO.BCM)      
-GPIO.setup(pin, GPIO.IN)
+GPIO.setmode(GPIO.BCM)       # GTIO BCM Mode (GPIO.BCM ja GPIO.BOARD erot löytyvät: https://raspberrypi.stackexchange.com/questions/12966/what-is-the-difference-between-board-and-bcm-for-gpio-pin-numbering)
+GPIO.setup(pin, GPIO.IN)     # GPIO IN
 
 print "Paina Ctrl+C lopetakseen."
 time.sleep(2)
 print "Valmis..";
 
-# Remote Terminal Koodi
+# Remote Terminal Koodi (Hakee tietokannasta tai script/pi_shell.php sivulta tekstiä jossa on tietokannassa commander taulukossa on command_read = 0)
+# Eli jos script/pi_shell.php sivustolle ilmentyy joku teksti ja se on command_read = '0' niin se suorittaa sen komentona ja päivittää 
+# myös script/pi-shell.php tiedostossa command_read = 1 joka ei näy enään sivulla.
+# script/pi_shell_response.php hakee tietokannasta komentoja tai teksitä jossa on command_read = 1 ja päivittää siihen vastauksen(response) &resonse=VASTAUS
+# jonka jälkeen se päivittää myös command_read = 2 (valmis)
 def updates():
     threading.Timer(3.0, updates).start() # timer :: toistuu aina 3 sek välein
     
@@ -32,7 +36,7 @@ def updates():
     
     # toteutetaan komento raspberrillä
     shell = subprocess.Popen(f.text, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
-    response = shell.read() # luetaan shell
+    response = shell.read() # luetaan teksti (response)
     #print response
     
     # skripti joka palauttaa viestin äskeiselle komennolla joka toteutettiin
@@ -54,28 +58,29 @@ try:
             camera.capture('/home/pi/Desktop/havainnot/' + date + '.png')
 
             # tässä muutetaan kuvan kokoa jotta dashboard toimisi mahdollisimman nopeasti
-            fd_img = open('/home/pi/Desktop/havainnot/' + date + '.png', 'r')
-            img = Image.open(fd_img)
-            img = resizeimage.resize_cover(img, [480, 360])
-            img.save('/home/pi/Desktop/havainnot/' + date + '.png', img.format)
-            fd_img.close()
+            fd_img = open('/home/pi/Desktop/havainnot/' + date + '.png', 'r') # Haetaan kuva
+            img = Image.open(fd_img) # avataan kuva
+            img = resizeimage.resize_cover(img, [480, 360]) # muutetaan kuvan koko 480x360 (leveys x pituus)
+            img.save('/home/pi/Desktop/havainnot/' + date + '.png', img.format)  # tallennetaan kuva
+            fd_img.close() # suljetaan resizeimage
             
-            camera.vflip = True
+            camera.vflip = True # käänetään kuva
 
-            # FTP Lataus
+            # FTP Lataus (FTP tunnuksen root kansiona pitää olla applikaation uploads/ kansio ja 777 oikeudet jotta se voi
+            # ladata siihen kuvia.
             session = ftplib.FTP('website.com','FTP KÄYTTÄJÄNIMI','FTP SALASANA') # FTP Tiedot
-            file = open('/home/pi/Desktop/havainnot/' + date + '.png', 'rb')                 
-            session.storbinary('STOR ' + date + '.png', file)     
-            file.close()                                   
-            session.quit()
+            file = open('/home/pi/Desktop/havainnot/' + date + '.png', 'rb') # Haetaan raspberry:lta kuva /kansio/kansio/-"-/-"-/pvm.png             
+            session.storbinary('STOR ' + date + '.png', file) # ladataan kyseinen kuva ftp protokollalla uploads/ kansioon
+            file.close() # suljetaan tiedosto.                               
+            session.quit() # suljetaan FTP yhteys
             
             # skripti joka tallentaa tietokantaan ja myös ilmoittaa käyttäjille jolla on kyseinen appkey ja &img=KUVAN PVM.png
             requests.get('https://marcosraudkett.com/mvrclabs/code/scripts/admin/script/service.php?app_key=APPKEY&img=' + date + '.png').content
             
-            os.remove('/home/pi/Desktop/havainnot/' + date + '.png')
+            os.remove('/home/pi/Desktop/havainnot/' + date + '.png') # Poistetaan kuva omalta Raspberry:ltä. (säästääkseen tilaa)
         
         # odottaa 4 sek ennenkuin voi havaita uudelleen.
         time.sleep(4)
 except KeyboardInterrupt:
-    print "Dead"
+    print "Dead" # jos keyboardilla painetaan CTRL+C niin ohjelma lopetetaan.
     GPIO.cleanup()
