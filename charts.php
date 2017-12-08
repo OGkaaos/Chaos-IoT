a217758362d5ff8caec5d6425566fd027426fae1
Marcos Raudkett <info@marcosraudkett.com>
2017-12-08 09:13:37 +0200

    Update charts.php

--- charts.php
+++ charts.php
@@ -1,8 +1,6 @@
 <?php 
  require_once("db.php"); /* tietokanta */
-
  if(isset($_COOKIE['ID_my_site'])){ /* tarkistetaan onko käyttyäjällä cookie ID_my_site olemassa */
-
         /* HUOM! Ei turvallinen tapa luoda tunnuksia tämä pitäisi vaihtaa esimerkiksi session key systeemiin 
         + salasanat hashata (lue readme.txt) ethän käytä tätä kirjautumistyyliä missään  */
         $email = $_COOKIE['ID_my_site'];  /* COOKIE["ID_my_site"] = Käyttäjänimi */
@@ -11,18 +9,15 @@
         $check = mysql_query("SELECT * FROM users WHERE email = '$email'") or die(mysql_error());
         $email = mysql_real_escape_string($_POST['email']);
         $res = mysql_query("SELECT * FROM users WHERE email='$email'");
-
     /* haetaan tietokannasta $check */
     while($info = mysql_fetch_array( $check ))   { 
         /* jos salasana ei ole sama kuin tietokannassa niin lähetetään käyttäjä takaisin signin.php sivulle */
         if ($pass != $info['password']) {           
                 header("Location: signin"); 
             } else { 
-
     /* käyttäjän email */
     $emailget = $_COOKIE[ID_my_site];
     $get = mysql_query("SELECT * FROM users WHERE email = '$emailget'");
-
     while ($row = mysql_fetch_assoc($get)) {
         /* luodaan käyttäjän tiedoille parametrit jotka haetaan $get query:istä */
         $firstname = $row["firstname"]; /* käyttäjän etunimi */
@@ -30,16 +25,12 @@
         $emailaddr = $row["email"]; /* käyttäjän sähköposti */
         $app_key = $row["app_key"]; /* käyttäjän app key */
     }
-
     setlocale(LC_TIME, array('fi_FI.UTF-8','fi_FI@euro','fi_FI','finnish')); 
     date_default_timezone_set('Europe/Helsinki');
-
-
 ?> <!--DOCTYPE html -->
 
 
 <?php 
-
 /* 
     @README *DONE 21.11.2017*
     tämän scriptin voisi luoda paremmin sillai että tehtäis yksi function jossa haettais aina -1 päivä jatkuvasti
@@ -47,7 +38,6 @@
     Jos tekis sillai niin olisi mahdollista myös lisätä sivulle asetus josta voi vaihtaa aikaväliä esimerkiksi kun tämä
     skripti hetkellä toimii VAIN 6 edellisen päivän kanssa..
 */
-
 /* 
   esim $secondday -1 päivä nykyisestä eli jos nyt olisi maanantai niin haettais sunnuntaita.
   ucwords() -> muuttaa ensimmäisen kirjaimen isoksi seuraavasti: maanantai -> Maanantai
@@ -55,10 +45,11 @@
          
 /* Function joka toimii loputtomasti(?) voi rajata vaikka koko vuoden */
 function tilastot($dates, $amount) {
-  /* tilastot funktio $dates = päivämäärät ja $amount = määrä */
+  /* tilastot funktio $dates = kaikki päivämäärät rajauksen välillä 
+  ja $amount = määrä päivämääriä yhteensä 
+  */
   $new = str_replace(' ',',', $dates); /* lisätään päivämäärien välille pilkku */
   $box_array = explode(',', $new);
-
   $i = 0; /* lasku on 0 */
   if($i != $amount) {
     if (is_array($box_array) || is_object($box_array))  {
@@ -67,7 +58,6 @@ function tilastot($dates, $amount) {
         $emailget = $_COOKIE[ID_my_site];
         $get = mysql_query("SELECT * FROM users WHERE email = '$emailget'");
         while ($row = mysql_fetch_assoc($get)) { $app_key = $row["app_key"]; }
-
        if($amount >= 60) { 
          /* tähän voi muokata tilastonnäkymää(?) että esim jos käyttäjä on rajannut enemmän kuin 60 pv niin sitten tulostuu kk eikä päivät */
          /* haetaan kyseisen päivän -1 päivä $amount */
@@ -79,17 +69,22 @@ function tilastot($dates, $amount) {
          $date_date = ucwords(strftime('%d.%m', strtotime($this_date, '-'.$amount.' day')));
          $date_fulldate = ucwords(strftime('%d.%m.%Y', strtotime($this_date, '-'.$amount.' day')));
        }
-
        /* kyseinen päivä kello 00:00:00 - 23:59:59 */
        $date_mysql = ucwords(strftime('%Y-%m-%d 00:00:00', strtotime($this_date, '-'.$amount.' day')));
        $date_mysql_2 = ucwords(strftime('%Y-%m-%d 23:59:59', strtotime($this_date, '-'.$amount.' day')));
-       /* tehdään tietokanta query jossa kello on $date_mysql ja $date_mysql_2 välillä */
+       /* tehdään tietokanta query jossa kello on $date_mysql ja $date_mysql_2 välillä kyseisellä päivällä */
        $get = mysql_query("SELECT * FROM notifications WHERE notification_appkey = '$app_key' AND notification_time BETWEEN '$date_mysql' AND '$date_mysql_2' ORDER BY notification_id DESC");
        $get_rows = mysql_num_rows($get);
        if($date_fulldate != '01.01.1970') { /* jos pvm on väärä niin ei tulosteta */
-         echo "['".$date_day." (".$date_date.")', ".$get_rows."],"; /* tulostetaan kaavaan */
+        /* 
+          tulostetaan kaavaan 
+          $date_day = päivä esim. Maanantai, 
+          $date_date = päivämäärä esim. 01.12, 
+          $get_rows = määrä havaintoja 
+        */
+         echo "['".$date_day." (".$date_date.")', ".$get_rows."],"; 
        }
-       $i++; /* lisätään jokaisen pvm kohdalla +1 laskuriin */
+       $i++; /* lisätään jokaisen pvm kohdalla +1 laskuriin eli kun $amount on saapunut tai funktio on suoritettu $amount monta kertaa niin tätä ei enään jatketa. */
      }
     }
   }
@@ -101,9 +96,6 @@ $current_date = strftime( '%d.%m' ); date_default_timezone_set('Europe/Helsinki'
 $current_fulldate = strftime( '%d.%m.%Y' ); date_default_timezone_set('Europe/Helsinki');
 $currentday_mysqlfulldate_1 = strftime( '%Y-%m-%d 00:00:00' ); date_default_timezone_set('Europe/Helsinki');
 $currentday_mysqlfulldate = strftime( '%Y-%m-%d 23:59:59' ); date_default_timezone_set('Europe/Helsinki');
-
-
-
 ?>
 
 
@@ -114,7 +106,6 @@ $get = mysql_query("SELECT * FROM settings");
 while ($row = mysql_fetch_assoc($get)) {
     $amount = $row["setting_amount"];
 }
-
 $current = mysql_query("SELECT * FROM notifications WHERE notification_appkey = '$app_key' AND notification_time BETWEEN '$currentday_mysqlfulldate_1' AND '$currentday_mysqlfulldate' ORDER BY notification_id DESC");
 $current_rows = mysql_num_rows($current);
          
@@ -149,7 +140,6 @@ while ($row = mysql_fetch_assoc($get)) {
    <script type="text/javascript">
       google.charts.load('current', {'packages':['line']});
       google.charts.setOnLoadCallback(drawChart);
-
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
           /* tässä tulostetaan seuraavasti ['Maanantai (20.07)', MÄÄRÄ] */
@@ -157,17 +147,14 @@ while ($row = mysql_fetch_assoc($get)) {
           <?php
             $from = $_GET["from"];
             $to = $_GET["to"];
-
             /* haetaan päivämäärät $from ja $to välillä */
             $period = new DatePeriod(
                new DateTime($from),
                new DateInterval('P1D'),
                new DateTime($to)
             );
-
             $begin = new DateTime($from);
             $end = new DateTime($to);
-
             $daterange = new DatePeriod($begin, new DateInterval('P1D'), $end);
             
             /* lasketaan kuinka monta päivämäärää $from ja $to välillä on (määrä) esim 01.11 - 07.11 = 6 */
@@ -178,19 +165,15 @@ while ($row = mysql_fetch_assoc($get)) {
                 /* tilastot funktio $dates = päivämäärät ja $amount = määrä */
                 tilastot($dates, $amount);
             }            
-
           ?>
           ['<?php echo 'Tänään ('.$current_date.')'; ?>',  <?php echo $current_rows; ?>],
         ]);
-
         var options = {
           title: 'Edellisen 7 päivän havainnot',
           curveType: 'function',
           legend: { position: 'bottom' }
         };
-
         var chart = new google.charts.Line(document.getElementById('line_top_x'));
-
         chart.draw(data, options);
       }
     </script>
@@ -264,7 +247,4 @@ $("#datepicker1").datepicker();
  /* jos käyttäjä ei olekkaan kirjautunut niin se lähetetään kirjautumissivulle. */         
  echo '<script>window.location.href = "../signin";</script>';
  } 
-
-
-
  ?> 
